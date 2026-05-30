c6b65ec9c3341d03709d2ac5ca2e172cd39ba026
Pyt-git <weicky0804@gmail.com>
2026-05-30 11:33:05 -0400

    Update test-Student.java

--- tests/test-Student.java
+++ tests/test-Student.java
@@ -4,5 +4,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class StudentTest {
 
   void testConstructorInitializesFields() {
-    Student s = new Student("Alice", 
+    Student s = new Student("Alice", 85); 
+    assertEquals(85, s.passed());
+  }
+
+  void testPassingGrade() {
+    Student s = new Student("Robert", 96); 
+    assertEquals(96, s.passed());
+  }
+
+  void testFailingGrade() {
+    Student s = new Student("Liliana", 59); 
+    assertEquals(59, s.passed());
+  }
+
+  void testMixedStatusGrades() {
+    Student s1 = new Student("Alex", 76); 
+    Student s2 = new Student("Nathan", 57); 
+    Student s3 = new Student("Ashley", 93); 
+
+    assertEquals(76, s1.passed()); // true
+    assertEquals(57, s2.passed()); // false
+    assertEquals(93, s3.passed()); // true
 
