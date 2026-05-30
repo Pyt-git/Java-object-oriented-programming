9489e2e3bc28416c8dec92706e198002b986c22f
Pyt-git <weicky0804@gmail.com>
2026-05-30 11:22:57 -0400

    Update test-Student.java

--- tests/test-Student.java
+++ tests/test-Student.java
@@ -1,1 +1,8 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+  void testConstructorInitializesFields() {
+    Student s = new Student
 
