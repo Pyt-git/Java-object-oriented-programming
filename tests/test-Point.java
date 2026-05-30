117e088b9f43711c4bbd66c5cac44ad9e74f0f8e
Pyt-git <weicky0804@gmail.com>
2026-05-30 11:19:44 -0400

    Update test-Point.java

--- tests/test-Point.java
+++ tests/test-Point.java
@@ -1,1 +1,30 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+  void testConstructorInitializesFields() {
+    Point p = new Point(3, 4);
+    assertEquals(3, p.x);
+    assertEquals(4, p.y);
+  }
+
+  void testMoveChangesCoordinates() {
+    Point p = new Point(1, 2);
+    p.move(6, 5);
+
+    assertEquals(7, p.x);
+    assertEquals(7, p.y); 
+  }
+
+  void testMultipleMoves() {
+    Point p = new Point(2, -3); 
+    p.move(2, 3); // (4, 0)
+    p.move(0, -7); // (4, -7)
+
+    assertEquals(4, p.x); 
+    assertEquals(-7, p.y);
+  }
+}
+    
 
